4b7b8ad2e365a36e3c0d0768bb0fdbfc18e73e38
Yasin Cakal <whoisyasin@gmail.com>
2023-07-04 14:12:45 +0300

    safe point 1

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,5 +1,6 @@
 import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
@@ -10,17 +11,23 @@ const Product = ({ product }) => {
           variant='top'
         />
       </Link>
+
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as='div'>
+          <Card.Title
+            as='div'
+            className='product-title'>
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+
         <Card.Text as='div'>
-          <div className='my-3'>
-            {product.rating} from {product.numReviews} reviews
-          </div>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
+
         <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
     </Card>
